ee4d82b65945c1fa4c1c2235cdc3bc7163f5db1a
Wojciech <wojciech.pryszcz@wp.pl>
2023-04-26 12:24:36 +0200

    changes with redirect

--- js/quiz.js
+++ js/quiz.js
@@ -9,5 +9,6 @@ let intervalId = setInterval(() => {
   if (countdown < 0) {
     clearInterval(intervalId);
     timerElement.textContent = "Time is up!";
+    window.location.href = 'ranking5.html'
   }
 }, 1000);
