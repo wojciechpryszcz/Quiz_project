f931a31ebbabb0e0ee1ab4cc861914d88ddd1795
Wojciech Pryszcz <wojciech.pryszcz@wp.pl>
2023-04-27 10:04:56 +0200

    fix const intervalId

--- js/quiz.js
+++ js/quiz.js
@@ -3,12 +3,12 @@ const countdownDuration = 30;
 const timerElement = document.getElementById("timer");
 
 let countdown = countdownDuration;
-let intervalId = setInterval(() => {
+const intervalId = setInterval(() => {
   timerElement.textContent = countdown;
   countdown--;
   if (countdown < 0) {
     clearInterval(intervalId);
     timerElement.textContent = "Time is up!";
-    window.location.href = 'ranking5.html'
+    window.location.href = "ranking5.html";
   }
 }, 1000);
